5f8d810ebb05976d9bdf340a5b0e990d2ef1c800
Steven Peniche <stevenmpeniche@gmail.com>
2017-11-02 10:59:02 -0400

    Assignment 4 - Models: complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,4 +17,14 @@ class AddressBook
 		end
 		entries.insert(index, Entry.new(name, phone_number, email))
 	end
+
+	def remove_entry(name, phone_number, email)
+		entries.each do |entry|
+			if (entry.name == name &&
+					entry.phone_number == phone_number &&
+					entry.email == email)
+				entries.delete(entry)
+			end
+		end
+	end
 end
